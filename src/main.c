3adc047e53d4f42c9b6f20fd87b434b57b856ce7
ming-body7 <gmbody7@gmail.com>
2015-01-14 16:15:24 -0500

    init
    pebble app which get speed alter from openxc

---
+++ src/main.c
@@ -0,0 +1,77 @@
+#include <pebble.h>
+#define KEY_DATA 1
+  
+Window *my_window;
+TextLayer *text_layer;
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Message received!");
+    // Get the first pair
+  Tuple *t = dict_read_first(iterator);
+
+  // Process all pairs present
+  while (t != NULL) {
+    // Long lived buffer
+    static char s_buffer[64];
+
+    // Process this pair's key
+    switch (t->key) {
+      case KEY_DATA:
+        // Copy value and display
+        snprintf(s_buffer, sizeof(s_buffer), "Received '%s'", t->value->cstring);
+        text_layer_set_text(text_layer, s_buffer);
+        break;
+    }
+
+    // Get next pair, if any
+    t = dict_read_next(iterator);
+  }
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+
+void handle_init(void) {
+  my_window = window_create();
+	text_layer = text_layer_create(GRect(0, 0, 144, 154));
+	
+	// Set the text, font, and text alignment
+	text_layer_set_text(text_layer, "Don't Overspeed!");
+	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+	
+	// Add the text layer to the window
+	layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(text_layer));
+
+	// Push the window
+	window_stack_push(my_window, true);
+  
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  
+}
+
+void handle_deinit(void) {
+  text_layer_destroy(text_layer);
+  window_destroy(my_window);
+}
+
+int main(void) {
+  handle_init();
+  app_event_loop();
+  handle_deinit();
+}
